252a3c760cbc8263022b2c6aad05465ec5c5805d
Welliton Gonçalves <wellitonaz@yahoo.com.br>
2022-03-04 13:54:04 -0300

    Estrutura base

---
+++ src/Pages/Header/index.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Header = styled.div`
+    display: flex;
+    justify-content: start;
+    align-items: center;
+
+    padding-left: 2rem;
+    
+    width: 100%;
+    height: 4rem;
+
+    background: #0080B2;
+    box-shadow: 0px 1px 3px rgba(0,0,0, 0.7);
+
+    color: #fff;
+    font-weight: bold;
+`;
+
+export default Header;
